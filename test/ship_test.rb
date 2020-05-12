e484143be76d8326c614498f6178464b217ec5da
Amy Moller <amy.moller@yahoo.com>
2020-05-12 15:03:38 -0600

    Create ship class and test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -13,27 +13,22 @@ class ShipTest < Minitest::Test
   end
 
   def test_it_has_a_name
-    skip
     assert_equal 'Cruiser', @cruiser.name
   end
 
   def test_it_has_a_length
-    skip
     assert_equal 3, @cruiser.length
   end
 
   def test_it_has_health_equal_to_length
-    skip
     assert_equal 3, @cruiser.health
   end
 
   def test_it_is_not_sunk_by_default
-    skip
     refute @cruiser.sunk?
   end
 
   def test_when_hit_loses_1_health
-    skip
     @cruiser.hit
     assert_equal 2, @cruiser.health
 
@@ -42,13 +37,12 @@ class ShipTest < Minitest::Test
   end
 
   def test_it_is_sunk_when_health_is_0
-    skip
     @cruiser.hit
     @cruiser.hit
     refute @cruiser.sunk?
 
     @cruiser.hit
-    assert_equal nil, @cruiser.hit
+    assert_equal 0, @cruiser.health 
     assert @cruiser.sunk?
   end
 end
