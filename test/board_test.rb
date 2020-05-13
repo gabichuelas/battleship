63c311ff5d9593ee4909e92da792a7dee05b6b76
Gaby Mendez <gmendez90@gmail.com>
2020-05-12 22:35:18 -0400

    Add tests for actual ship placement on board

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,6 +7,8 @@ require './lib/board'
 class BoardTest < Minitest::Test
   def setup
     @board = Board.new()
+    @cruiser = Ship.new('Cruiser', 3)
+    @submarine = Ship.new('Submarine', 2)
   end
 
   def test_it_exists
@@ -22,6 +24,8 @@ class BoardTest < Minitest::Test
     assert_instance_of Cell, @board.cells["AI"].class
   end
 
+  # TEST VALID COORDINATES
+
   def test_it_has_a_valid_coordinate
     skip
     # checks if a coordinate is on the board or not
@@ -33,10 +37,10 @@ class BoardTest < Minitest::Test
     refute @board.valid_coordinate("A22")
   end
 
+  # TEST VALID PLACEMENTS
+
   def test_valid_placement_by_length
     skip
-    cruiser = Ship.new('Cruiser', 3)
-    submarine = Ship.new('Submarine', 2)
     # write statement that checks if ship placement
     # array of coordinates is equal in number to
     # length of ship.
@@ -45,28 +49,47 @@ class BoardTest < Minitest::Test
     # only 2 coordinates in the array (AI, A2) but
     # cruisers have a length of 3.
     # Same with submarine
-    refute @board.valid_placement?(cruiser, ["A1", "A2"])
-    refute @board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    refute @board.valid_placement?(@cruiser, ["A1", "A2"])
+    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
 
   def test_valid_placement_by_consecutive_cell_coordinates
-    refute @board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    refute @board.valid_placement?(submarine, ["A1", "C1"])
-    refute @board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    refute @board.valid_placement?(submarine, ["C1", "B1"])
+    refute @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    refute @board.valid_placement?(@submarine, ["A1", "C1"])
+    refute @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    refute @board.valid_placement?(@submarine, ["C1", "B1"])
   end
 
   def test_valid_placement_coordinates_not_diagonal
-    refute @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
-    refute @board.valid_placement?(submarine, ["C2", "D3"])
+    refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    refute @board.valid_placement?(@submarine, ["C2", "D3"])
   end
 
   def test_valid_ship_placement
-    assert @board.valid_placement?(submarine, ["A1", "A2"])
-    assert @board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+    assert @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 
+  # TEST PLACING SHIPS
 
+  def test_can_have_a_ship_placed_on_it
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+
+    assert_instance_of Cell, cell_1
+    assert_instance_of Cell, cell_2
+    assert_instance_of Cell, cell_3
+
+    assert_instance_of Ship, cell_1.ship
+    assert_instance_of Ship, cell_2.ship
+    assert_instance_of Ship, cell_3.ship
+
+    # assert_same checks for equivalent
+    # objects ID, not equivalent states
+    assert_same cell_3.ship, cell_2.ship
+  end 
 
 
 end
