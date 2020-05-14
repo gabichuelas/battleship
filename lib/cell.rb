654e783704e6b85de0db9ac60680c8bb3ded2d0e
Amy Moller <amy.moller@yahoo.com>
2020-05-13 18:13:44 -0600

    Add more method edits

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,44 @@
 class Cell
 
-  attr_reader :coordinates
+  attr_reader :coordinates,
+              :ship,
+              :fired_upon
 
   def initialize(coordinates)
     @coordinates = coordinates
+    @ship = nil
+    @fired_upon = false
+
   end
 
-  def render
-
+  def empty?
+    if @ship.class == Ship
+      false
+    else
+      true
+    end
   end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if @ship.class == Ship
+      @fired_upon = true
+      @ship.hit
+    else
+    end
+  end
+
+
+  def render()
+    "."
+    #if/elsif/else/end
+  end
+#optional parameters
 end
