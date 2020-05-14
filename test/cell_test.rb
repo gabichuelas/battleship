d03a2b145888889ddcbda95219f710659a9bb8e4
Amy Moller <amy.moller@yahoo.com>
2020-05-14 15:08:05 -0600

    Add finishing test for cell

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,7 +6,6 @@ require './lib/cell'
 class CellTest < Minitest::Test
 
   def setup
-
     @cell = Cell.new("B4")
   end
 
@@ -58,43 +57,70 @@ class CellTest < Minitest::Test
   def test_cell_can_be_rendered
 
     cell_1 = Cell.new("B4")
-    #refute @cell.fired_upon?
     assert_equal ".", cell_1.render
   end
 
   def test_fired_upon_and_shot_missed
-    skip
+
     cell_1 = Cell.new("B4")
     cell_1.fire_upon
     assert_equal "M", cell_1.render
   end
 
-  def test_cell_rendering_with_ship_hit
-    skip
+  def test_cell_rendering_with_ship
+
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_2.place_ship(cruiser)
     assert_equal ".", cell_2.render
     assert_equal "S", cell_2.render(true)
+  end
 
+  def test_cell_rendering_with_hit
+    # we created a new cell
+    cell_2 = Cell.new("C3")
+    # we fire_upon the cell BEFORE there's a ship in it
     cell_2.fire_upon
-    assert @cell.fired_upon?
-    assert_equal "H", cell_2.render
+    # which means cell.fired_upon? == true
+    # BUT the cell would render as "M" bc there was no ship
 
-    refute @cruiser.sunk?
+    # let's create a ship
+    cruiser = Ship.new("Cruiser", 3)
+    # let's put the ship on the cell_2
+    cell_2.place_ship(cruiser)
+    # what if we wanted to render the cell WITH the ship
+    # on it, but before Firing on it again? The cell
+    # should show as "." or "S"
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+    # so fired_upon? is now false, because we reset
+    # the fired_upon status to false with
+    # the place_ship method
+    refute cell_2.fired_upon?
+    # BUT, according to our rendering rules, the cell would
+    # show H even though we never fired on the cell AFTER
+    # we placed the ship, which obv doesn't work when playing.
+
+    # let's fire on the cell now, AFTER we placed a ship
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+    # cruiser hasn't sunk because we only hit one of the
+    # 3 cells it occupies
+    refute cruiser.sunk?
   end
 
   def test_it_is_sunk_when_hit_3_times
-    skip
+
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_2.place_ship(cruiser)
 
-    @cruiser.hit
-    @cruiser.hit
-    refute @cruiser.sunk?
-    @cruiser.hit
-    assert @cruiser.sunk?
+    cruiser.hit
+    cruiser.hit
+    refute cruiser.sunk?
+
+    cruiser.hit
+    assert cruiser.sunk?
     assert_equal "X", cell_2.render
   end
 
