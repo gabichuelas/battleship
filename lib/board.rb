aead73ed415cbc5763a5f2cb6e4aff27ac662503
Amy Moller <amy.moller@yahoo.com>
2020-05-14 15:37:54 -0600

    Initial commit

---
+++ lib/board.rb
@@ -0,0 +1,24 @@
+class Board
+
+  attr_reader :cells,
+              :coordinates,
+              :ship,
+
+
+
+  def initialize (cells, )
+    @cells = cells
+    @coordinates = coordinates
+    @ship = 
+
+
+
+  end
+
+
+
+
+
+
+
+end
