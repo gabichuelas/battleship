48c8702a75ba0c0e3ec1d8e4e65989143248238a
Amy Moller <amy.moller@yahoo.com>
2020-05-13 18:12:49 -0600

    Add more test edits

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,56 +6,57 @@ require './lib/cell'
 class CellTest < Minitest::Test
 
   def setup
-    skip
+
     @cell = Cell.new("B4")
   end
 
   def test_it_exists
-    skip
+
     assert_instance_of Cell, @cell
   end
 
   def test_it_has_coordinates
-    skip
-    assert_equal "B4", @cell.coordinate
+
+    assert_equal "B4", @cell.coordinates
   end
 
   def test_it_does_not_have_a_ship
-    skip
-    assert_equal nil, @cell.ship
+
+    assert_nil @cell.ship
   end
 
   def test_it_is_empty
-    skip
+
     assert @cell.empty?
   end
 
   def test_it_has_a_new_ship
-    skip
+
     cruiser = Ship.new("Cruiser", 3)
     @cell.place_ship(cruiser)
-    assert_instance_of Ship, cell.ship
+    assert_instance_of Ship, @cell.ship
     refute @cell.empty?
   end
 
   def test_it_has_not_been_fired_upon
-    skip
+
     cruiser = Ship.new("Cruiser", 3)
     @cell.place_ship(cruiser)
     refute @cell.fired_upon?
   end
 
   def test_has_been_fired_upon
-    skip
+
     cruiser = Ship.new("Cruiser", 3)
     @cell.place_ship(cruiser)
     @cell.fire_upon
+
     assert_equal 2, @cell.ship.health
     assert @cell.fired_upon?
   end
 
   def test_cell_can_be_rendered
-    skip
+
     cell_1 = Cell.new("B4")
     assert_equal ".", cell_1.render
   end
