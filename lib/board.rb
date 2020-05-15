fa3c47a5b64d1715592b0d83294316c91d94306c
Amy Moller <amy.moller@yahoo.com>
2020-05-14 18:11:06 -0600

    Add more methods to class

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,41 @@
 class Board
 
-  attr_reader :cells,
-              :coordinates,
-              :ship,
+  def initialize
+  end
 
+  def cells
+    { "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4")
+    }
+  end
 
-
-  def initialize (cells, )
-    @cells = cells
-    @coordinates = coordinates
-    @ship = 
-
-
+  def valid_coordinate?(coordinate)
+    cells.any? do |cell_name, cell|
+      coordinate == cell_name
+    end
 
   end
 
+  def valid_placement?(ship, coordinates)
 
+    (ship.length == coordinates.length) &&
+    (cells.each_cons(3).to_a.flatten == coordinates)
 
-
+  end
 
 
 
