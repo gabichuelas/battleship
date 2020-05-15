280dd14d17d3c8fcc6ef25589627a9509ba6b3a0
Gaby Mendez <gmendez90@gmail.com>
2020-05-15 18:17:49 -0400

    Edit valid_placement? method

--- lib/board.rb
+++ lib/board.rb
@@ -27,16 +27,45 @@ class Board
     cells.any? do |cell_name, cell|
       coordinate == cell_name
     end
-
   end
 
   def valid_placement?(ship, coordinates)
+    columns = coordinates.map { |c| c[1].to_i }
+    # turns given coordinates into array of integers
+    rows = coordinates.map { |c| c[0].ord }
+    # turns given coordinates into array of letter.ordinal_values
 
-    (ship.length == coordinates.length) &&
-    (cells.each_cons(3).to_a.flatten == coordinates)
+    horizontal_check = (1..4).each_cons(ship.length).any?(columns)
+    # vertical check
+    letters = ("A".."D").to_a.map { |a| a.ord }
+    vertical_check = letters.each_cons(ship.length).any?(rows)
 
+    if horizontal_check || vertical_check && ship.length == coordinates.length
+      true
+    else
+      false
+    end
+
+
+
+    # cell_keys = cells.keys
+    # ["A1", "A2", ...]
+
+    # cell_key_nums = cells.keys.map { |key| key.to_i }
+    # [1, 2, ...]
+
+
+    # the following works if it's horizontal consecutiveness
+    # but not vertical
+    # if cell_keys.each_cons(ship.length).any?(coordinates) &&
+    #   ship.length == coordinates.length
+    #   true
+    # else
+    #   false
+    # end
+    # if we check for (1,2,3) && (A1, A
+
+    # if we check for same # && consecutive cells according to ord = vertical check
   end
-
-
 
 end
