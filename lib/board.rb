5769ff1b0a9f05d71747e13066ad2eeec19cebcd
Gaby Mendez <gmendez90@gmail.com>
2020-05-15 18:56:00 -0400

    Cleanup validation method

--- lib/board.rb
+++ lib/board.rb
@@ -44,43 +44,18 @@ class Board
     letters_ord = ("A".."D").to_a.map { |a| a.ord }
     vertical_check = letters_ord.each_cons(ship.length).any?(rows)
 
-    # if horizontal_check || vertical_check && ship.length == coordinates.length
-    #   true
-    # else
-    #   false
-    # end
-    # require "pry"; binding.pry
     if ship.length == coordinates.length
       if horizontal_check && coordinate_letters.all?(coordinate_letters[0])
         true
       elsif vertical_check && !coordinate_letters.all?(coordinate_letters[0]) && columns.all?(columns[0])
         true
       else
-        false 
+        false
       end
     else
       false
     end
 
-
-    # cell_keys = cells.keys
-    # ["A1", "A2", ...]
-
-    # cell_key_nums = cells.keys.map { |key| key.to_i }
-    # [1, 2, ...]
-
-
-    # the following works if it's horizontal consecutiveness
-    # but not vertical
-    # if cell_keys.each_cons(ship.length).any?(coordinates) &&
-    #   ship.length == coordinates.length
-    #   true
-    # else
-    #   false
-    # end
-    # if we check for (1,2,3) && (A1, A
-
-    # if we check for same # && consecutive cells according to ord = vertical check
   end
 
 end
