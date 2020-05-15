291317b6123f7c1d780c40f49fab1c76d65c8733
Gaby Mendez <gmendez90@gmail.com>
2020-05-15 00:05:17 -0400

    Syntax cleanup

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -10,41 +10,34 @@ class CellTest < Minitest::Test
   end
 
   def test_it_exists
-
     assert_instance_of Cell, @cell
   end
 
   def test_it_has_coordinates
-
     assert_equal "B4", @cell.coordinates
   end
 
   def test_it_does_not_have_a_ship
-
     assert_nil @cell.ship
   end
 
   def test_it_is_empty
-
     assert @cell.empty?
   end
 
   def test_it_has_a_new_ship
-
     cruiser = Ship.new("Cruiser", 3)
     @cell.place_ship(cruiser)
     refute @cell.empty?
   end
 
   def test_it_has_not_been_fired_upon
-
     cruiser = Ship.new("Cruiser", 3)
     @cell.place_ship(cruiser)
     assert_equal false, @cell.fired_upon?
   end
 
   def test_has_been_fired_upon_and_loses_1_health
-
     cruiser = Ship.new("Cruiser", 3)
     @cell.place_ship(cruiser)
     @cell.fire_upon
@@ -54,20 +47,17 @@ class CellTest < Minitest::Test
   end
 
   def test_cell_can_be_rendered
-
     cell_1 = Cell.new("B4")
     assert_equal ".", cell_1.render
   end
 
   def test_fired_upon_and_shot_missed
-
     cell_1 = Cell.new("B4")
     cell_1.fire_upon
     assert_equal "M", cell_1.render
   end
 
   def test_cell_rendering_with_ship
-
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_2.place_ship(cruiser)
@@ -107,7 +97,6 @@ class CellTest < Minitest::Test
   end
 
   def test_it_is_sunk_when_hit_3_times
-
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_2.place_ship(cruiser)
@@ -124,5 +113,4 @@ class CellTest < Minitest::Test
     assert_equal true, cruiser.sunk?
     assert_equal "X", cell_2.render
   end
-
 end
