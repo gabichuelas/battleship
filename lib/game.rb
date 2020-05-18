384a0db9fdfc3e7024070aa135ee1043990fb159
Amy Moller <amy.moller@yahoo.com>
2020-05-18 12:51:38 -0600

    Add quick syntax fix

--- lib/game.rb
+++ lib/game.rb
@@ -15,20 +15,20 @@ class Game
     You now need to lay out your two ships.
     The Cruiser is three units long and the Submarine is two units long."
 
-      1 2 3 4
-    A . . . .
-    B . . . .
-    C . . . .
-    D . . . .
+#      1 2 3 4
+#    A . . . .
+#    B . . . .
+#    C . . . .
+#    D . . . .
 
-    "Enter the squares for the Cruiser (3 spaces):"
+    puts "Enter the squares for the Cruiser (3 spaces):"
     print "> "
 
 
     #new board showing user-placed Cruiser
 
 
-    "Enter the squares for the Submarine (2 spaces):"
+    puts "Enter the squares for the Submarine (2 spaces):"
     print "> "
 
     #if user enters invalid sequence(s), extra prompts~
