868785852123183e17dd768cb0e839b369c5a987
Amy Moller <amy.moller@yahoo.com>
2020-05-20 14:32:54 -0600

    Add final tests for completion

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
@@ -15,14 +16,15 @@ class GameTest < Minitest::Test
     assert_instance_of Game, @game
   end
 
+  def test_has_a_menu
+    @game.stubs(:gets).returns("q\n")
+
+    assert_output(/You've quit the game./) { @game.main_menu }
+  end
+
   def test_it_has_comp_and_player_boards
     assert_instance_of Board, @game.comp_board
     assert_instance_of Board, @game.player_board
-  end
-
-  def test_has_a_menu
-    skip
-    assert @game.main_menu
   end
 
   def test_has_ships
@@ -35,9 +37,8 @@ class GameTest < Minitest::Test
     assert_instance_of Ship, @game.player_ships['Cruiser']
   end
 
+  def test_can_pick_right_num_of_coordinates
 
-  def test_can_pick_random_coordinates
-    # skip
     @game.generate_ships
 
     cells = @game.random_coordinates(@game.comp_ships['Cruiser'])
@@ -45,15 +46,28 @@ class GameTest < Minitest::Test
     assert_equal 3, cells.count
   end
 
-  def test_can_place_computer_ships
-    skip
-    @game.computer_ships_placement
-    # can't test this bc output changes
-    # every time, but I verified it in pry
-    # by puts -ing the following line
-    @game.comp_board.render
-    @game.comp_board.render(true)
+  def test_comp_ships_can_sink
+    @game.generate_ships
+    @game.comp_ships['Cruiser'].hit
+    @game.comp_ships['Cruiser'].hit
+    @game.comp_ships['Cruiser'].hit
+    @game.comp_ships['Submarine'].hit
+    @game.comp_ships['Submarine'].hit
+    assert_equal true, @game.comp_ships_sunk?
   end
+
+  def test_player_ships_can_sink
+    @game.generate_ships
+    @game.player_ships['Cruiser'].hit
+    @game.player_ships['Cruiser'].hit
+    @game.player_ships['Cruiser'].hit
+    @game.player_ships['Submarine'].hit
+    @game.player_ships['Submarine'].hit
+    assert_equal true, @game.player_ships_sunk?
+  end
+
+
+
 
 
 
